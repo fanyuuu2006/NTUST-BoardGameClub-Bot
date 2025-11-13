b32c36b12cd162f69748da422f4bf1dc8278f06b
Yorn90104 <bingxiao526@gmail.com>
2025-11-13 16:31:40 +0800

    feat: 新增靜態檔案服務，並更新根路由以回傳 index.html

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@
 import express from "express";
 import { router as webhookRouter } from "./routers/webhook";
 import { router as assetsRouter } from "./routers/assets";
+import path from "path";
 
 const app = express();
 app.use("/webhook", webhookRouter);
 app.use("/assets", assetsRouter);
+
+app.use(express.static(path.join(__dirname, "../public")));
 app.use("/", (_, res) => {
-  res.send(`Server is running`);
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
 
 export default app;
